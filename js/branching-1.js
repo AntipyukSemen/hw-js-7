7a3dbe302b062e84483dc8881a5271dbdc3965ed
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-04-03 09:50:00 +0300

    hw-js-7

---
+++ js/branching-1.js
@@ -0,0 +1,22 @@
+const lang = "ua";
+
+switch (lang) {
+  case "ua":
+    console.log("Січень");
+    break;
+
+  case "en":
+    console.log("January");
+    break;
+
+  case "ru":
+    console.log("");
+    break;
+
+  case "fr":
+    console.log("Janvier");
+    break;
+
+  default:
+    console.log("Неможливо перевести");
+};
